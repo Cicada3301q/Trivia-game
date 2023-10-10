70a03ec043db17624e96ce68ef87d990363ccfb4
Quenten <quentenwelch@gmail.com>
2023-10-09 18:47:42 -0600

    Initial project boiler plate set up

---
+++ Trivia-Game/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import './BackgroundStyles.css';
+import { Button, Stack } from "@mui/material";
+import "./App.css";
+
+function App() {
+  const options = [
+    { label: "Security", category: "Security" },
+    { label: "Privacy", category: "Privacy" },
+    { label: "Authentication", category: "Authentication" },
+    { label: "Passwords", category: "Passwords" },
+    { label: "Authentication", category: "Authentication" },
+    { label: "Malware", category: "Malware" },
+  ];
+
+  return (
+    <div>
+      <h1>Information Security Trivia!</h1>
+      <Stack spacing={2}>
+        {options.map((option) => (
+          <Button
+            size="medium"
+            variant="contained"
+            onClick={() => {
+              alert(option.category);
+            }}
+          >
+            {option.label}
+          </Button>
+        ))}
+      </Stack>
+    </div>
+  );
+}
+
+export default App;
